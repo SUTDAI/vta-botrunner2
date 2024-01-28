c6cf74f844f89e126b73f8736a99eacaf76b3f83
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-01-28 17:37:58 +0800

    feat: proof of concept

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,64 @@
-import { Elysia } from "elysia";
+import {
+  GenerationConfig,
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
+  SafetySetting,
+} from '@google/generative-ai'
+import { Elysia, t } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// Refer to https://ai.google.dev/tutorials/node_quickstart
+// Also check out https://elysiajs.com/validation/overview.html
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const MODEL_NAME = 'gemini-pro'
+const API_KEY = process.env.GOOGLEAI_KEY
+
+const safetySettings: SafetySetting[] = [
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+]
+
+const generationConfig: GenerationConfig = {
+  candidateCount: 1,
+  temperature: 0.9,
+  topP: 1,
+  topK: 1,
+}
+
+const genAI = new GoogleGenerativeAI(API_KEY)
+const model = genAI.getGenerativeModel({
+  model: MODEL_NAME,
+  safetySettings,
+  generationConfig,
+})
+
+const app = new Elysia()
+
+app.post(
+  '/generate',
+  async ({ body }) => {
+    const { prompt } = body
+    const { response } = await model.generateContent(prompt)
+
+    return {
+      text: response.text(),
+    }
+  },
+  { body: t.Object({ prompt: t.String() }) },
+)
+
+app.listen(3000)
---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+declare module 'bun' {
+  interface Env {
+    /** Google AI Studio API key (used for Gemini). */
+    GOOGLEAI_KEY: string
+  }
+}
